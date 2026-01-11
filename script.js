7cb58304ffca5fdb410056dcc7ee8e293d0846f1
Todd Marino <todd.j.marino@gmail.com>
2026-01-11 16:26:16 -0600

    css & img swap on thumbnail clicks

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const quantityButtons = document.querySelectorAll('[data-button]');
 const itemQuantity = document.querySelector('#item-quantity');
 
+const thumbnailImgs = document.querySelectorAll('.thumbnail')
+
 let quantity = 0;
 
 quantityButtons.forEach((btn) => {
@@ -14,3 +16,18 @@ quantityButtons.forEach((btn) => {
     itemQuantity.textContent = quantity;
   });
 });
+
+thumbnailImgs.forEach((thumbnail) => {
+    // Get the data-thumbnail value/number
+    thumbnail.addEventListener('click', () => {
+        const img = thumbnail.querySelector('img');
+        const number = img.dataset.thumbnail;
+        const featuredPhoto = document.querySelector('.featured-photo')
+        const featuredImg = featuredPhoto.querySelector('img')
+        featuredImg.setAttribute('src', `images/image-product-${number}.jpg`)
+
+        thumbnailImgs.forEach((thumbnail) => thumbnail.classList.remove('selected-img'));
+
+        thumbnail.classList.add('selected-img')
+    })
+})
